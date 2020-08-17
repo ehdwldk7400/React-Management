2d17c9c6cfbbdccd90af9521a2441f2bf64b8380
ehdwldk7400 <68597164+ehdwldk7400@users.noreply.github.com>
2020-08-17 18:14:10 +0900

    map 함수이용 배열 출력

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import React, { Component } from "react";
 import "./App.css";
 import Customer from "./compoents/Customer";
 
-const customer = {
-  name: "양동진",
-  birthday: "961222",
-  gender: "남자",
-  job: "대학생",
-};
+const customer = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1", //랜덤으로 이미지를 보여주는 사이트
+    name: "양동진",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2", //랜덤으로 이미지를 보여주는 사이트
+    name: "나동진",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3", //랜덤으로 이미지를 보여주는 사이트
+    name: "이동진",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {customer.map((c) => {
+          return (
+            <Customer
+              key={c.id} // map이라는 메서드를 이용시에는 key값을 설정해줘야하고 중복되지 않는값으로 정의해주면된다.
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
