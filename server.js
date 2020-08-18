6b602b7ff465357efc159564f377cc1a77349455
ehdwldk7400 <68597164+ehdwldk7400@users.noreply.github.com>
2020-08-18 23:08:16 +0900

    Create Customer Dtabase Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,24 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ exteded: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+connection.connect();
+
 app.get("/api/customer", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1", //랜덤으로 이미지를 보여주는 사이트
-      name: "양동진",
-      birthday: "961222",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2", //랜덤으로 이미지를 보여주는 사이트
-      name: "나동진",
-      birthday: "961222",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3", //랜덤으로 이미지를 보여주는 사이트
-      name: "이동진",
-      birthday: "961222",
-      gender: "남자",
-      job: "대학생",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, field) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
